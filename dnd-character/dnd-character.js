70a18109f9a2ac4ffb5a2ec2197db954886556d3
narcisolobo <narcisolobo@gmail.com>
2024-05-20 10:16:27 -0700

    dnd character - easy

--- dnd-character/dnd-character.js
+++ dnd-character/dnd-character.js
@@ -1,8 +1,13 @@
-//
+// @ts-check
 // This is only a SKELETON file for the 'D&D Character' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
+/**
+ *
+ * @param {number} constitution the character's constitution
+ * @returns {number}
+ */
 export const abilityModifier = (constitution) => {
   if (constitution < 3) {
     throw new Error('Ability scores must be at least 3');
@@ -11,61 +16,53 @@ export const abilityModifier = (constitution) => {
     throw new Error('Ability scores can be at most 18');
   }
 
-  constitution -= 10;
-  constitution /= 2;
-  constitution = Math.floor(constitution);
-  return constitution;
+  return Math.floor((constitution - 10) / 2);
 };
 
 export class Character {
+  static rollDie = () => Math.floor(Math.random() * 6) + 1;
+
   static rollAbility() {
-    let rolls = [];
-    for (let i = 1; i <= 4; i++) {
-      rolls.push(Math.floor(Math.random() * 6) + 1);
-    }
-    rolls = rolls.sort();
-    let sum = 0;
-    for (let i = 1; i < rolls.length; i++) {
-      sum += rolls[i];
-    }
-    return sum;
+    let rolls = Array.from({ length: 4 }, this.rollDie);
+    const [_, ...highest] = rolls.sort();
+    return highest.reduce((total, roll) => total + roll);
   }
 
   constructor() {
-    this.strength = Character.rollAbility();
-    this.dexterity = Character.rollAbility();
-    this.constitution = Character.rollAbility();
-    this.intelligence = Character.rollAbility();
-    this.wisdom = Character.rollAbility();
-    this.charisma = Character.rollAbility();
-    this.hitpoints = 10 + abilityModifier(this.constitution);
+    this._strength = Character.rollAbility();
+    this._dexterity = Character.rollAbility();
+    this._constitution = Character.rollAbility();
+    this._intelligence = Character.rollAbility();
+    this._wisdom = Character.rollAbility();
+    this._charisma = Character.rollAbility();
+    this._hitpoints = 10 + abilityModifier(this._constitution);
   }
 
   get strength() {
-    return this.strength;
+    return this._strength;
   }
 
   get dexterity() {
-    return this.dexterity;
+    return this._dexterity;
   }
 
   get constitution() {
-    return this.constitution;
+    return this._constitution;
   }
 
   get intelligence() {
-    return this.intelligence;
+    return this._intelligence;
   }
 
   get wisdom() {
-    return this.wisdom;
+    return this._wisdom;
   }
 
   get charisma() {
-    return this.charisma;
+    return this._charisma;
   }
 
   get hitpoints() {
-    return this.hitpoints;
+    return this._hitpoints;
   }
 }
